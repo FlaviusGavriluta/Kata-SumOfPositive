44e3ed765d7ea37b59126652ae29e53ae60a4f11
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-08-03 00:02:11 +0300

    verified the sum of Positive numbers

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,16 @@
 package org.example;
 
+import static java.lang.System.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("The sum of positive numbers is: " + SumOfPositive.sumOfPositives(new int[]{1, -2, 3, 4, 5}));
+        int[] arr1 = {1, -4, 7, 12};
+        out.println("The sum of positive numbers is: " + SumOfPositive.sumOfPositives(arr1)); // 20
+
+        int[] arr2 = {-1, -2, -3, -4, -5};
+        out.println("The sum of positive numbers is: " + SumOfPositive.sumOfPositives(arr2)); // 0
+
+        int[] arr3 = {1, 2, 3, 4, 5};
+        out.println("The sum of positive numbers is: " + SumOfPositive.sumOfPositives(arr3)); // 15
     }
 }
